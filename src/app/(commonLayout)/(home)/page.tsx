876ef671b230f86245110802eb77f4136d0ef185
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-04-16 15:30:03 +0600

    image upload part completed

--- src/app/(commonLayout)/(home)/page.tsx
+++ src/app/(commonLayout)/(home)/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+
+interface IImageUpload {
+  event: "success";
+  info: {
+    public_id: string;
+  };
+}
 export default function Home() {
-  return <div>Home</div>;
+  const [imageId, setImageId] = useState("y9xmk7sjcvctxhqef2rb");
+  return (
+    <main>
+      <div className="flex justify-center items-center min-h-screen">
+        <CldUploadWidget
+          uploadPreset="image-gallery"
+          onSuccess={(results) => {
+            const res = results as IImageUpload;
+            // console.log("Public ID", res.info.public_id);
+            setImageId(res.info.public_id);
+          }}
+        >
+          {({ open }) => {
+            return (
+              <Button variant="outlined" onClick={() => open()}>
+                Upload
+              </Button>
+            );
+          }}
+        </CldUploadWidget>
+      </div>
+      <div>
+        <CldImage
+          width="400"
+          height="400"
+          src={imageId}
+          sizes="80vw"
+          alt="Description of my image"
+        />
+      </div>
+    </main>
+  );
 }
--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <AppRouterCacheProvider>{children}</AppRouterCacheProvider>;
+  return <div>{children}</div>;
 }
